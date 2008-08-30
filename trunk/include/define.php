288b42d6e80023a8402ee88da094261ef6a4e73a
gnuzip <gnuzip@70708d55-9555-0410-a691-c731ea5ce794>
2008-08-30 09:56:44 +0000

    defining global const

---
+++ trunk/include/define.php
@@ -0,0 +1,37 @@
+<?php
+
+$define_tagslist=array(
+//    id,   name,               if_controlled)
+array(1,   'type',	        true),
+array(2,   'genre',             true),
+array(3,   'topic',             true),
+array(4,   'title',             true),
+array(5,   'republisher',       true),
+array(6,   'translator',        true),
+array(7,   'artist',            true),
+array(8,   'contributor',       true),
+array(9,   'language',          true),
+array(10,  'subtitle_language', true),
+array(11,  'origin',            true),
+array(12,  'format',            true),
+array(13,  'subtitle_format',   true),
+array(14,  'video_codec',       true),
+array(15,  'video_height',      true),
+array(16,  'software_platform', true),
+array(17,  'part_number',       false),
+array(18,  'video_height',      false),
+array(19,  'video_width',       false),
+array(20,  'video_bitrate',     false),
+array(21,  'video_framerate',   false),
+array(22,  'audio_bitrate',     false),
+array(23,  'audio_channel',     false),
+array(24,  'date_air',          false),
+array(25,  'date_republish',    false),
+array(26,  'size',              false),
+array(27,  'republish_title',   false),
+array(28,  'republish_comment', false),
+array(29,  'torrent_infohash',  false),
+array(30,  'torrent_filelist',  false),
+array(31,  'torrent_comment',   false),
+);
+?>
