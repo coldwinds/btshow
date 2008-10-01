88ef48abd2e9969d59195d7135cb232dbee13419
cs17899219 <cs17899219@70708d55-9555-0410-a691-c731ea5ce794>
2008-10-01 17:05:59 +0000

    TorrentComponent

---
+++ trunk/app/controllers/components/torrent_component.php
@@ -0,0 +1,11 @@
+<?php
+class TorrentComponent extends Object {
+	function parse_file($file_name){
+		// some codes return array();
+	}
+
+	function addannounce($url){
+		// some codes return true;
+	}
+}
+?>
