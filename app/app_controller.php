e3bd671fb244788a60082735a89ea254081df171
ahui.no1 <ahui.no1@70708d55-9555-0410-a691-c731ea5ce794>
2008-10-08 01:28:24 +0000

    更改用户密码加密方式为md5

--- app/app_controller.php
+++ app/app_controller.php
@@ -2,9 +2,11 @@
 class AppController extends Controller {
 	// acl support
 	var $components = array('Auth', 'Acl');
-	
+
 	function beforeFilter() {
 		//Configure AuthComponent
+		$this->Auth->userModel	=	'User';
+		$this->Auth->authenticate	=	& $this->Auth->getModel();//默认为userModel
 		$this->Auth->authorize = 'actions';
 		$this->Auth->actionPath = 'controllers/';
 		$this->Auth->loginAction = array('controller' => 'users', 'action' => 'login');
