271a75b4b973752ebed140602601ace76ab1dae4
cs17899219 <cs17899219@70708d55-9555-0410-a691-c731ea5ce794>
2008-10-11 09:55:43 +0000

    1.调整了torrent index 和 torrent hot的结构 2.合并torrent的数据展现视图

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -13,17 +13,12 @@ class UsersController extends AppController {
 
 		//allow managers to posts and widgets
 		$group->id = 2;
-		$this->Acl->deny($group, 'controllers');
-		$this->Acl->allow($group, 'controllers/Posts');
-		$this->Acl->allow($group, 'controllers/Widgets');
+		$this->Acl->allow($group, 'controllers');
 
 		//allow users to only add and edit on posts and widgets
 		$group->id = 3;
-		$this->Acl->deny($group, 'controllers');
-		$this->Acl->allow($group, 'controllers/Posts/add');
-		$this->Acl->allow($group, 'controllers/Posts/edit');
-		$this->Acl->allow($group, 'controllers/Widgets/add');
-		$this->Acl->allow($group, 'controllers/Widgets/edit');
+		$this->Acl->allow($group, 'controllers');
+
 	}
 
 	/**
